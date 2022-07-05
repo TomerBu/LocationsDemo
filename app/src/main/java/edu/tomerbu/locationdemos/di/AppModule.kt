9adbde28268f812e04bad37fa3be5c6b9dcc7f36
BuTomer <bu.tomer@gmail.com>
2022-07-05 11:16:46 +0300

    initial commit

---
+++ app/src/main/java/edu/tomerbu/locationdemos/di/AppModule.kt
@@ -0,0 +1,38 @@
+package edu.tomerbu.locationdemos.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
+import edu.tomerbu.locationdemos.SharedLocationManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+/**
+ * Configuration for DI on the repository and shared location manager
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideCoroutineScope(): CoroutineScope {
+        return CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSharedLocationManager(
+        @ApplicationContext context: Context,
+        coroutineScope: CoroutineScope
+    ): SharedLocationManager =
+        SharedLocationManager(context,coroutineScope)
+}
+
