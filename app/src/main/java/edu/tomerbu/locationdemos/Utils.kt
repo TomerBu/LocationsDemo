a02b793935d65a189eb4fe9014fa70a6cdde6a0f
BuTomer <bu.tomer@gmail.com>
2022-07-07 12:08:56 +0300

    background location permission

--- app/src/main/java/edu/tomerbu/locationdemos/Utils.kt
+++ app/src/main/java/edu/tomerbu/locationdemos/Utils.kt
@@ -3,8 +3,15 @@ package edu.tomerbu.locationdemos
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.edit
+import androidx.fragment.app.Fragment
+import edu.tomerbu.locationdemos.ui.TAG
 
 /**
  * Returns the `location` object as a human readable string.
@@ -28,7 +35,8 @@ fun Context.hasPermission(permission: String): Boolean {
 
     // Background permissions didn't exit prior to Q, so it's approved by default.
     if (permission == Manifest.permission.ACCESS_BACKGROUND_LOCATION &&
-        android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.Q) {
+        android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.Q
+    ) {
         return true
     }
 
@@ -36,12 +44,48 @@ fun Context.hasPermission(permission: String): Boolean {
             PackageManager.PERMISSION_GRANTED
 }
 
+fun Context.hasPreciseLocation(): Boolean =
+    hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+fun Fragment.hasPreciseLocation(): Boolean =
+    requireContext().hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+fun Context.hasCoarseLocation(): Boolean =
+    hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+fun Fragment.hasCoarseLocation(): Boolean =
+    requireContext().hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+fun Context.hasLocationPermission(): Boolean =
+    hasPreciseLocation() or hasCoarseLocation()
+
+fun Fragment.hasLocationPermission(): Boolean =
+    requireContext().hasLocationPermission()
+
+fun Context.hasBackgroundPermission(): Boolean =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        hasPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+    } else {
+        true
+    }
+
+fun Fragment.hasBackgroundPermission(): Boolean =
+    requireContext().hasBackgroundPermission()
+
+
+fun Fragment.requestPermissionLauncher(callback: (Boolean) -> Unit) =
+    registerForActivityResult(ActivityResultContracts.RequestPermission(), callback)
+
+fun Fragment.requestPermissionsLauncher(callback: (Map<String, Boolean>) -> Unit) =
+    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions(), callback)
+
 /**
  * Provides access to SharedPreferences for location to Activities and Services.
  */
 internal object SharedPreferenceUtil {
 
     const val KEY_FOREGROUND_ENABLED = "tracking_foreground_location"
+    const val KEY_DID_REQUEST_PERMISSION_ALREADY = "DidRequestPermissionAlready"
 
     /**
      * Returns true if requesting location updates, otherwise returns false.
@@ -50,7 +94,8 @@ internal object SharedPreferenceUtil {
      */
     fun getLocationTrackingPref(context: Context): Boolean =
         context.getSharedPreferences(
-            context.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+            context.getString(R.string.preference_file_key), Context.MODE_PRIVATE
+        )
             .getBoolean(KEY_FOREGROUND_ENABLED, false)
 
     /**
@@ -60,7 +105,27 @@ internal object SharedPreferenceUtil {
     fun saveLocationTrackingPref(context: Context, requestingLocationUpdates: Boolean) =
         context.getSharedPreferences(
             context.getString(R.string.preference_file_key),
-            Context.MODE_PRIVATE).edit {
-                putBoolean(KEY_FOREGROUND_ENABLED, requestingLocationUpdates)
-            }
+            Context.MODE_PRIVATE
+        ).edit {
+            putBoolean(KEY_FOREGROUND_ENABLED, requestingLocationUpdates)
+        }
+
+    fun getDidRequestPermissionAlready(context: Context): Boolean =
+        context.getSharedPreferences(
+            context.getString(R.string.preference_file_key),
+            Context.MODE_PRIVATE
+        )
+            .getBoolean(KEY_DID_REQUEST_PERMISSION_ALREADY, false)
+
+    /**
+     * Stores the location updates state in SharedPreferences.
+     * @param requestingLocationUpdates The location updates state.
+     */
+    fun saveDidRequestPermissionAlready(context: Context, requestingLocationUpdates: Boolean) =
+        context.getSharedPreferences(
+            context.getString(R.string.preference_file_key),
+            Context.MODE_PRIVATE
+        ).edit {
+            putBoolean(KEY_DID_REQUEST_PERMISSION_ALREADY, requestingLocationUpdates)
+        }
 }
--- app/src/main/java/edu/tomerbu/locationdemos/ui/FirstFragment.kt
+++ app/src/main/java/edu/tomerbu/locationdemos/ui/FirstFragment.kt
@@ -1,18 +1,26 @@
 package edu.tomerbu.locationdemos.ui
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import androidx.work.OneTimeWorkRequest
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.Fragment
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import edu.tomerbu.locationdemos.R
+import edu.tomerbu.locationdemos.*
 import edu.tomerbu.locationdemos.databinding.FragmentFirstBinding
-import edu.tomerbu.locationdemos.work.DemoWorker
 import edu.tomerbu.locationdemos.work.RefreshDataWorker
+import kotlin.system.exitProcess
 
 
 /**
@@ -36,12 +44,57 @@ class FirstFragment : Fragment() {
 
     }
 
+    private val launcher: ActivityResultLauncher<String> = requestPermissionLauncher {
+        if (it) {
+            Log.d(TAG, "onCreate: We have fine or coarse")
+            doWorkThatRequiresPermission()
+        } else {
+            Log.d(TAG, "onCreate: No Permission")
+            AlertDialog.Builder(requireContext()).setCancelable(false)
+                .setNegativeButton("Bye") { d, w ->
+                    Toast.makeText(requireContext(), "Bye", Toast.LENGTH_SHORT).show()
+                    exitProcess(0)
+                }
+                .setPositiveButton("Settings") { d, w ->
+                    startActivity(
+                        Intent(
+                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:" + BuildConfig.APPLICATION_ID)
+                        )
+                    )
+                }.setTitle("Settings or quit").show()
+        }
+    }
+
+    private fun doWorkThatRequiresPermission() {
+        if (!hasLocationPermission()) {
+            launcher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            return
+        }
+        //background must be requested separately
+        if (!hasBackgroundPermission() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                AlertDialog.Builder(requireContext()).setCancelable(false)
+                    .setNegativeButton("Cancel") { _, _ -> }
+                    .setPositiveButton("Background") { _, _ ->
+                        launcher.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                    }.setTitle("Please give us background permissions")
+                    .setMessage("Settings -> allow all the time").show()
+
+            } else {
+                launcher.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            }
+            return
+        }
+        Toast.makeText(requireContext(), "We have background", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.buttonFirst.setOnClickListener {
-//            val request = OneTimeWorkRequestBuilder<DemoWorker>().build()
-//            WorkManager.getInstance(requireContext()).enqueue(request)
+            doWorkThatRequiresPermission()
+            //val request = OneTimeWorkRequestBuilder<DemoWorker>().build()
+            //WorkManager.getInstance(requireContext()).enqueue(request)
 
             val request = OneTimeWorkRequestBuilder<RefreshDataWorker>().build()
             WorkManager.getInstance(requireContext()).enqueue(request)
